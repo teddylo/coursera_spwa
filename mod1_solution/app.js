6f3d8cac7c6334f13611dd002b64e3fd1b10217b
teddy <teddylo@gmail.com>
2020-09-28 06:09:16 +0000

    mod1_solution

---
+++ mod1_solution/app.js
@@ -0,0 +1,24 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope' ];
+function LunchCheckController() {
+	$scope.items = "";
+	$scope.msg="";
+	$scope.checkIfTooMuch = function () {
+		if (scope.items.length == 0) {
+			$scope.msg="Please enter data first";
+		}
+		else if ($scope.items.split(',').length <= 3) {
+			$scope.msg="Enjoy!";
+		}
+		else {
+			$scope.msg="Too much!";
+		}
+  	};
+};
+
+})();
